4b99b0f8cd5dda5a4372dc092585e25c9f34a206
An T.G <boltcarrier@naver.com>
2022-11-10 01:11:46 +0900

    first commit

---
+++ src/main/java/com/example/pharmacyrecommendationproject/PharmacyRecommendationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyrecommendationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommendationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyRecommendationProjectApplication.class, args);
+    }
+
+}
